ac1ce5962dfcf39ae12e887ec631f9045bb130f4
Vikas92-dot <vikasmathe143@gmail.com>
2025-02-12 20:44:32 +0530

    Creating Student Management System using States

---
+++ Student_App/src/components/Data.jsx
@@ -0,0 +1,44 @@
+export default [
+    {
+        roll: 1,
+        name: "vikas",
+        branch: "IT",
+        gender: "Male",
+        location: "Indore"
+    },
+    {
+        roll: 2,
+        name: "goutam",
+        branch: "CS",
+        gender: "Male",
+        location: "Dewas"
+    },
+    {
+        roll: 3,
+        name: "pooja",
+        branch: "CV",
+        gender: "Female",
+        location: "Guna"
+    },
+    {
+        roll: 4,
+        name: "Lily",
+        branch: "IT",
+        gender: "Female",
+        location: "Indore"
+    },
+    {
+        roll: 5,
+        name: "teena",
+        branch: "CV",
+        gender: "Female",
+        location: "Dewas"
+    },
+    {
+        roll: 6,
+        name: "cheeku",
+        branch: "Mech",
+        gender: "Male",
+        location: "Indore"
+    }
+]
---
+++ Student_App/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import { Component } from "react";
+
+class Header extends Component{
+    render(){
+        return<>
+        <div
+          className="d-flex justify-content-center align-items-center shadow-lg"
+          style={{
+            height: "70px",
+            background: "linear-gradient(135deg, #ff416c, #ff4b2b)",
+            color: "white",
+            fontWeight: "bold",
+            fontSize: "24px",
+            letterSpacing: "1px",
+            textTransform: "uppercase",
+          }}
+        >
+          Student Management System
+        </div>
+        </>
+    }
+}
+export default Header;
